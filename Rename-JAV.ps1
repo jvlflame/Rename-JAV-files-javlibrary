4765009f6a1358d1c1b398323a02022cbb4c2430
jvlflame <jli141928@gmail.com>
2019-06-17 18:22:24 -0700

    Update Rename-JAV.ps1

--- Rename-JAV.ps1
+++ Rename-JAV.ps1
@@ -327,7 +327,3 @@ function Rename-JAV {
         }
         pause
     }
-
-$ScriptRoot = [System.AppDomain]::CurrentDomain.BaseDirectory.TrimEnd('\')
-
-Rename-JAV -FilePath $ScriptRoot -FileSize 250 -LogPath (Join-Path -Path $ScriptRoot -ChildPath rename-jav-log.txt)
